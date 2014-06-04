a32de715f954c78e3d519b84b875ff0d91e4269a
Schlueter <schlueter@bschlueter.com>
2014-06-04 03:26:51 -0400

    Add amazon_ses cookbook

---
+++ cookbooks/amazon_ses/metadata.rb
@@ -0,0 +1,9 @@
+name             'amazon_ses'
+maintainer       'Schlueter'
+maintainer_email 'bs@bschlueter.com'
+license          'GPL v3'
+description      'Sends mail with Amazon SES'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.0.1'
+
+supports         'ubuntu'
